b33be74dd9bcb51f27f8309134281b9cc80e7fab
Tom <tom.canac@gmail.com>
2017-04-06 09:04:46 -0400

    Eslint init

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,54 @@
-const webpack = require("webpack");
-const path = require("path");
+const webpack = require('webpack');
+const path = require('path');
 const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 
 const PROD = process.env.NODE_ENV === 'production';
 
 const config = {
-    entry: {
-        app: path.join(__dirname, 'src/app.js')
+  entry: {
+    app: path.join(__dirname, 'src/app.js'),
+  },
+  output: {
+    path: path.join(__dirname, '.build/js/'),
+    publicPath: 'js/',
+    filename: '[name].js',
+    chunkFilename: '[name].[chunkhash].js',
+    libraryTarget: 'var',
+    library: 'App',
+  },
+  devtool: PROD ? 'source-map' : 'eval',
+  watch: !PROD,
+  cache: false,
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      loader: 'babel-loader',
     },
-    output: {
-        path: path.join(__dirname, '.build/js/'),
-        publicPath: "js/",
-        filename: "[name].js",
-        chunkFilename: "[name].[chunkhash].js",
-        libraryTarget: 'var',
-        library: 'App'
-    },
-    devtool: PROD ? 'source-map' : 'eval',
-    watch: !PROD,
-    cache: false,
-    module: {
-        loaders: [{
-            test: /\.js$/,
-            exclude: /(node_modules)/,
-            loader: 'babel-loader'
-        },
-        {
-            test: /\.less$/,
-            use: [
-                'style-loader',
+    {
+      test: /\.less$/,
+      use: [
+        'style-loader',
                 { loader: 'css-loader', options: { importLoaders: 1 } },
-                'less-loader'
-            ]
-        },
-        {
-            test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-            loader: 'file-loader'
-        }]
+        'less-loader',
+      ],
     },
-    plugins: [],
-    resolve: {
-        modules: [
-            path.join(__dirname, "node_modules"),
-            path.join(__dirname, "src")
-        ]
-    }
+    {
+      test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+      loader: 'file-loader',
+    }],
+  },
+  plugins: [],
+  resolve: {
+    modules: [
+      path.join(__dirname, 'node_modules'),
+      path.join(__dirname, 'src'),
+    ],
+  },
 };
 
-if(PROD) {
-    config.plugins.push(new UglifyJsPlugin({}))
+if (PROD) {
+  config.plugins.push(new UglifyJsPlugin({}));
 }
 
 module.exports = config;
